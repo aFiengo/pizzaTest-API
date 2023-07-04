717f0521a7eb0d02fc70f5a9fa64c2bde2d5293a
Ariel_Fiengo <ariel.fiengo@truextend.com>
2023-07-04 14:28:42 -0400

    Modified the Managers and IManager classes to implement the IMapper and use the DTOs models

--- Truextend/PizzaTest/Logic/Managers/Interface/IToppingManager.cs
+++ Truextend/PizzaTest/Logic/Managers/Interface/IToppingManager.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Truextend.PizzaTest.Data.Models;
+using Truextend.PizzaTest.Logic.Models;
 
 namespace Truextend.PizzaTest.Logic.Managers.Interface
 {
     public interface IToppingManager
     {
-        Task<IEnumerable<Topping>> GetAllAsync();
-        Task<Topping> GetByIdAsync(Guid id);
-        Task<Topping> CreateAsync(Topping topping);
-        Task<Topping> UpdateAsync(Guid id, Topping toppingToUpdate);
+        Task<IEnumerable<ToppingDTO>> GetAllAsync();
+        Task<ToppingDTO> GetByIdAsync(Guid id);
+        Task<ToppingDTO> CreateAsync(ToppingDTO topping);
+        Task<ToppingDTO> UpdateAsync(Guid id, ToppingDTO toppingToUpdate);
         Task<bool> DeleteAsync(Guid id);
     }
 }
--- Truextend/PizzaTest/Logic/Managers/ToppingManager.cs
+++ Truextend/PizzaTest/Logic/Managers/ToppingManager.cs
@@ -3,48 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Truextend.PizzaTest.Core.Exceptions;
 using Truextend.PizzaTest.Data.Models;
 using Truextend.PizzaTest.Data;
 using Truextend.PizzaTest.Logic.Managers.Base;
+using Truextend.PizzaTest.Logic.Managers.Interface;
+using AutoMapper;
+using Truextend.PizzaTest.Logic.Models;
+using Truextend.PizzaTest.Core.Exceptions;
 
 namespace Truextend.PizzaTest.Logic.Managers
 {
-    public class ToppingManager : IGenericManager<Topping>
+    public class ToppingManager : IToppingManager
     {
         private readonly IUnitOfWork _uow;
+        private readonly IMapper _mapper;
 
-        public ToppingManager(IUnitOfWork unitOfWork)
+        public ToppingManager(IUnitOfWork unitOfWork, IMapper mapper)
         {
             _uow = unitOfWork;
+            _mapper = mapper;
         }
 
-        public async Task<IEnumerable<Topping>> GetAllAsync()
+        public async Task<IEnumerable<ToppingDTO>> GetAllAsync()
         {
-            return await _uow.ToppingRepository.GetAllAsync();
+            var toppings = await _uow.PizzaRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<ToppingDTO>>(toppings);
         }
-        public async Task<Topping> GetByIdAsync(Guid id)
+        public async Task<ToppingDTO> GetByIdAsync(Guid id)
         {
-            return await _uow.ToppingRepository.GetByIdAsync(id);
+            var topping =  await _uow.ToppingRepository.GetByIdAsync(id);
+            return _mapper.Map<ToppingDTO>(topping);
         }
-        public async Task<Topping> CreateAsync(Topping topping)
+        public async Task<ToppingDTO> CreateAsync(ToppingDTO toppingToAdd)
         {
-            return await _uow.ToppingRepository.CreateAsync(topping);
+            var topping = _mapper.Map<Topping>(toppingToAdd);
+            var createdTopping = await _uow.ToppingRepository.CreateAsync(topping);
+            return _mapper.Map<ToppingDTO>(createdTopping);
         }
-        public async Task<Topping> UpdateAsync(Guid id, Topping toppingToUpdate)
+        public async Task<ToppingDTO> UpdateAsync(Guid id, ToppingDTO toppingToUpdate)
         {
-            Topping topping = await _uow.ToppingRepository.GetByIdAsync(id);
-            if (topping != null)
+            var existingTopping = await _uow.ToppingRepository.GetByIdAsync(id);
+            if (existingTopping == null)
             {
-                topping.Name = toppingToUpdate.Name;
-
+                throw new NotFoundException($"Topping with ID {id} not found.");
             }
-            return await _uow.ToppingRepository.UpdateAsync(toppingToUpdate);
+
+            var updatedTopping = _mapper.Map<Topping>(toppingToUpdate);
+            existingTopping.Name = updatedTopping.Name;
+
+            var result = await _uow.ToppingRepository.UpdateAsync(existingTopping);
+            return _mapper.Map<ToppingDTO>(result);
         }
         public async Task<bool> DeleteAsync(Guid id)
         {
-            Topping toppingFound = await _uow.ToppingRepository.GetByIdAsync(id);
-            await _uow.ToppingRepository.DeleteAsync(toppingFound);
+            var existingTopping = await _uow.ToppingRepository.GetByIdAsync(id);
+            if (existingTopping == null)
+            {
+                throw new NotFoundException($"Topping with ID {id} not found.");
+            }
+
+            await _uow.ToppingRepository.DeleteAsync(existingTopping);
             return await _uow.ToppingRepository.GetByIdAsync(id) == null;
         }
     }
