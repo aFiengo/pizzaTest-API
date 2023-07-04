858221e0748fdc3c29ac829e1f2d127a6b2b343e
Ariel_Fiengo <ariel.fiengo@truextend.com>
2023-07-04 10:46:40 -0400

    Finished the Application and IApplication classses and the DB connection string

---
+++ Truextend/PizzaTest/Configuration/ApplicationConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Truextend.PizzaTest.Configuration.Models;
+
+namespace Truextend.PizzaTest.Configuration
+{
+    public class ApplicationConfiguration
+    {
+        private readonly IConfiguration _configuration;
+        public ApplicationConfiguration(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public DatabaseConnectionString GetDatabaseConnectionString()
+        {
+            return new DatabaseConnectionString()
+            {
+                DATABASE = _configuration.GetSection("ConnectionStrings").GetSection("PizzaDB").Value
+            };
+        }
+    }
+}
---
+++ Truextend/PizzaTest/Configuration/Models/DatabaseConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Truextend.PizzaTest.Configuration.Models
+{
+    public class DatabaseConnectionString
+    {
+        public string DATABASE { get; set; }
+    }
+}
