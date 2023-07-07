6ec9910a0ba339fe3cbb164b26c7535246bb5500
Ariel_Fiengo <ariel.fiengo@truextend.com>
2023-07-07 14:12:04 -0400

    Added the scripts to create the DB

---
+++ Truextend/PizzaTest/Data/Scripts/pizzeriaDB_schemas.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS PizzeriaDB;
+CREATE DATABASE PizzeriaDB;
+
+USE PizzeriaDB;
+
+-- Create Pizza table
+CREATE TABLE Pizza (
+  Id CHAR(36) PRIMARY KEY,
+  Name VARCHAR(255),
+  SmallImageUrl VARCHAR(255),
+  LargeImageUrl VARCHAR(255)
+);
+
+-- Create Toppings table
+CREATE TABLE Topping (
+  Id CHAR(36) PRIMARY KEY,
+  Name VARCHAR(255)
+);
+
+-- Create PizzaToppings table
+CREATE TABLE PizzaTopping (
+  PizzaId CHAR(36),
+  ToppingId CHAR(36),
+  PRIMARY KEY (PizzaId, ToppingId),
+  FOREIGN KEY (PizzaId) REFERENCES Pizza(Id),
+  FOREIGN KEY (ToppingId) REFERENCES Topping(Id)
+);
+
+
+
+
+
