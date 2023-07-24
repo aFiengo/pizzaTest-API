35d9817f8017cee42b371d077fdd35143cd7df68
Ariel Fiengo <ariel.fiengo@truextend.com>
2023-07-24 13:52:10 -0400

    Finished the unit test for the Pizza repository

--- Truextend/PizzaTest/Pizza.UnitTests/PizzaRepositoryTests/PizzaRepositoryTests.cs
+++ Truextend/PizzaTest/Pizza.UnitTests/PizzaRepositoryTests/PizzaRepositoryTests.cs
@@ -12,22 +12,35 @@ using System.Xml.Linq;
 using Truextend.PizzaTest.Data.Repository;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Truextend.PizzaTest.Logic.Exceptions;
 
 namespace Truextend.PizzaTest.UnitTests.PizzaRepositoryTests
 {
     [TestFixture]
+    public class TestPizzaDbContext : PizzaDbContext
+    {
+        public TestPizzaDbContext(IApplicationConfiguration applicationConfiguration)
+        : base(applicationConfiguration)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("TestDb");
+        }
+    }
     public class PizzaRepositoryTests
     {
-        private static DbContextOptions<PizzaDbContext> _options = new DbContextOptionsBuilder<PizzaDbContext>()
+        private static DbContextOptions<TestPizzaDbContext> _options = new DbContextOptionsBuilder<TestPizzaDbContext>()
             .UseInMemoryDatabase(databaseName: "TestDb")
             .Options;
 
-        private PizzaDbContext _context;
+        private TestPizzaDbContext _context;
         private List<Pizza> _data;
         private IApplicationConfiguration _appConfiguration;
 
 
-        [OneTimeSetUp]
+        [SetUp]
         public void Setup()
         {
 
@@ -35,17 +48,17 @@ namespace Truextend.PizzaTest.UnitTests.PizzaRepositoryTests
             mockAppConfiguration.Setup(ac => ac.GetDatabaseConnectionString())
                 .Returns(new ConnectionStrings { DATABASE = "TestDb" });
 
-            _appConfiguration = mockAppConfiguration.Object; // Use this line to set _appConfiguration
+            _appConfiguration = mockAppConfiguration.Object; 
 
-            _context = new PizzaDbContext(_appConfiguration); // Pass _appConfiguration here
+            _context = new TestPizzaDbContext(_appConfiguration); 
             _context.Database.EnsureCreated();
 
             _data = new List<Pizza>
-        {
-            new Pizza { Id = Guid.NewGuid(), Name = "Pizza 1" },
-            new Pizza { Id = Guid.NewGuid(), Name = "Pizza 2" },
-            new Pizza { Id = Guid.NewGuid(), Name = "Pizza 3" }
-        };
+            {
+                new Pizza { Id = Guid.NewGuid(), Name = "Pizza 1", Description = "Desc 1", LargeImageUrl = "url1", SmallImageUrl = "url1" },
+                new Pizza { Id = Guid.NewGuid(), Name = "Pizza 2", Description = "Desc 2", LargeImageUrl = "url2", SmallImageUrl = "url2" },
+                new Pizza { Id = Guid.NewGuid(), Name = "Pizza 3", Description = "Desc 3", LargeImageUrl = "url3", SmallImageUrl = "url3" }
+            };
 
             _context.Pizza.AddRange(_data);
             _context.SaveChanges();
@@ -77,11 +90,124 @@ namespace Truextend.PizzaTest.UnitTests.PizzaRepositoryTests
             // Assert
             result.Should().BeEquivalentTo(pizzaToFind);
         }
+        [Test]
+        public async Task GetToppingsForPizzaAsync_ReturnsToppingsForGivenPizzaId()
+        {
+            // Arrange
+            var pizzaToFind = _data[0];
+            var toppingsForPizza = new List<Topping>
+            {
+                new Topping { Id = Guid.NewGuid(), Name = "Topping 1" },
+                new Topping { Id = Guid.NewGuid(), Name = "Topping 2" },
+                new Topping { Id = Guid.NewGuid(), Name = "Topping 3" }
+            };
 
-        [OneTimeTearDown]
-        public void CleanUp()
+            _context.Topping.AddRange(toppingsForPizza);
+            await _context.SaveChangesAsync();
+
+            pizzaToFind.Toppings = toppingsForPizza;
+            _context.Pizza.Update(pizzaToFind);
+            await _context.SaveChangesAsync();
+
+            var repository = new PizzaRepository(_context);
+
+            // Act
+            var result = await repository.GetToppingsForPizzaAsync(pizzaToFind.Id);
+
+            // Assert
+            result.Should().BeEquivalentTo(pizzaToFind.Toppings);
+        }
+
+        [Test]
+        public async Task AddToppingToPizzaAsync_AddsToppingToPizza()
+        {
+            // Arrange
+            var pizzaToUpdate = _data[1];
+            var toppingToAdd = new Topping { Id = Guid.NewGuid(), Name = "Locoto" };
+            _context.Topping.Add(toppingToAdd);
+            await _context.SaveChangesAsync();
+            var repository = new PizzaRepository(_context);
+
+            // Act
+            var result = await repository.AddToppingToPizzaAsync(pizzaToUpdate.Id, toppingToAdd.Id);
+
+            // Assert
+            result.Toppings.Should().Contain(toppingToAdd);
+        }
+
+        [Test]
+        public async Task CreateAsync_CreatesNewPizza()
+        {
+            // Arrange
+            var newPizza = new Pizza { Name = "New Pizza", Description = "This is a new pizza.", LargeImageUrl = "url4", SmallImageUrl = "url4" };
+            var repository = new PizzaRepository(_context);
+
+            // Act
+            var createdPizza = await repository.CreateAsync(newPizza);
+
+            // Assert
+            createdPizza.Should().NotBeNull();
+            createdPizza.Id.Should().NotBeEmpty();
+            createdPizza.Name.Should().Be(newPizza.Name);
+            createdPizza.Description.Should().Be(newPizza.Description);
+
+            // Verify that the pizza was added to the database
+            var pizzaInDb = await _context.Pizza.FindAsync(createdPizza.Id);
+            pizzaInDb.Should().NotBeNull();
+            pizzaInDb.Name.Should().BeEquivalentTo(newPizza.Name);
+        }
+
+        [Test]
+        public async Task DeleteAsync_DeletesPizza()
+        {
+            // Arrange
+            var pizzaToDelete = _data[1];
+            var repository = new PizzaRepository(_context);
+
+            // Act
+            await repository.DeleteAsync(pizzaToDelete);
+            await _context.SaveChangesAsync();
+
+            // Assert
+            var deletedPizza = await _context.Pizza.FindAsync(pizzaToDelete.Id);
+            deletedPizza.Should().BeNull();
+        }
+        [Test]
+        public async Task DeleteToppingFromPizzaAsync_RemovesToppingFromPizza()
+        {
+            // Arrange
+            var pizzaToUpdate = _data[0];
+            var toppingsForPizza = new List<Topping>
+            {
+                new Topping { Id = Guid.NewGuid(), Name = "Topping 1" },
+                new Topping { Id = Guid.NewGuid(), Name = "Topping 2" },
+                new Topping { Id = Guid.NewGuid(), Name = "Topping 3" }
+            };
+
+            _context.Topping.AddRange(toppingsForPizza);
+            await _context.SaveChangesAsync();
+
+            pizzaToUpdate.Toppings = toppingsForPizza;
+            _context.Pizza.Update(pizzaToUpdate);
+            await _context.SaveChangesAsync();
+
+            var repository = new PizzaRepository(_context);
+
+            // Act
+            var toppingToRemove = pizzaToUpdate.Toppings.First();
+            await repository.DeleteToppingFromPizzaAsync(pizzaToUpdate.Id, toppingToRemove.Id);
+
+            // Assert
+            var updatedPizza = await _context.Pizza.Include(p => p.Toppings).FirstOrDefaultAsync(p => p.Id == pizzaToUpdate.Id);
+            updatedPizza.Toppings.Should().NotContain(toppingToRemove);
+        }
+
+
+        [TearDown]
+        public void TearDown()
         {
             _context.Database.EnsureDeleted();
+            _context.Dispose();
         }
     }
 
